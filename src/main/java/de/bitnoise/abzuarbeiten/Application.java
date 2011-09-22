ed2609f8e3effb2c5c9bf8dba43b8b5574f22445
RainerW <git@inforw.de>
2011-09-22 21:50:02 +0200

    fixed errorn when no state xml

--- src/main/java/de/bitnoise/abzuarbeiten/Application.java
+++ src/main/java/de/bitnoise/abzuarbeiten/Application.java
@@ -15,52 +15,47 @@ import de.bitnoise.abzuarbeiten.model.Aufgabe;
 import de.bitnoise.abzuarbeiten.model.ITaskItem;
 import de.bitnoise.abzuarbeiten.model.Zustand;
 
-public class Application
-{
-  Zustand _zustand;
+public class Application {
+	Zustand _zustand;
 
-  List<IListener> _listeners = new ArrayList<IListener>();
+	List<IListener> _listeners = new ArrayList<IListener>();
 
-  public Application()
-  {
-    XStream xs = new XStream();
-    _zustand = (Zustand) xs.fromXML(new File("zustand.xml"));
-  }
+	public Application() {
+		XStream xs = new XStream();
+		File f = new File("zustand.xml");
+		if (f.exists()) {
+			_zustand = (Zustand) xs.fromXML(f);
+		} else {
+			_zustand = new Zustand();
+		}
+	}
 
-  public List<ITaskItem> getAufgaben()
-  {
-    return _zustand.getAufgaben();
-  }
+	public List<ITaskItem> getAufgaben() {
+		return _zustand.getAufgaben();
+	}
 
-  public void setAufgaben(List<ITaskItem> aufgaben)
-  {
-    _zustand = new Zustand();
-    _zustand.getAufgaben().addAll(aufgaben);
+	public void setAufgaben(List<ITaskItem> aufgaben) {
+		_zustand = new Zustand();
+		_zustand.getAufgaben().addAll(aufgaben);
 
-    XStream xs = new XStream();
-    try
-    {
-      FileOutputStream fo = new FileOutputStream("zustand.xml");
-      xs.toXML(_zustand, fo);
-    }
-    catch (FileNotFoundException e)
-    {
-      e.printStackTrace();
-    }
+		XStream xs = new XStream();
+		try {
+			FileOutputStream fo = new FileOutputStream("zustand.xml");
+			xs.toXML(_zustand, fo);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
-    fireEvent(new AufgabenListeChanged(aufgaben));
-  }
+		fireEvent(new AufgabenListeChanged(aufgaben));
+	}
 
-  private void fireEvent(AufgabenListeChanged aufgabenListeChanged)
-  {
-    for (IListener listener : _listeners)
-    {
-      listener.event(aufgabenListeChanged);
-    }
-  }
+	private void fireEvent(AufgabenListeChanged aufgabenListeChanged) {
+		for (IListener listener : _listeners) {
+			listener.event(aufgabenListeChanged);
+		}
+	}
 
-  public void addAufgabenChangeListener(IListener listener)
-  {
-    _listeners.add(listener);
-  }
+	public void addAufgabenChangeListener(IListener listener) {
+		_listeners.add(listener);
+	}
 }
